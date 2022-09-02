3d057ec55e0e5343c0aabc4cd648627369999d06
unknown <michaelabebe19922000@gmail.com>
2022-09-02 17:13:29 +0300

    Home page and Nav-bar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCsBH-lqihwtIiG4zf19K6vQ8twmshiOY8",
+    authDomain: "challenge-3e4a1.firebaseapp.com",
+    projectId: "challenge-3e4a1",
+    storageBucket: "challenge-3e4a1.appspot.com",
+    messagingSenderId: "773868411413",
+    appId: "1:773868411413:web:a9d4a8a536fbce608c0c1c"
+  };
