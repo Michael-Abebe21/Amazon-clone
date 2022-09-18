24f041be8282159f53e8b69d283f0487e4ac21b4
unknown <michaelabebe19922000@gmail.com>
2022-09-18 18:29:09 +0300

    login with firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Checkout from "./Checkout";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The user is >>> ", authUser);
+
+      if (authUser) {
+        //The user is logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //The user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     //BEM
     <Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,19 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCsBH-lqihwtIiG4zf19K6vQ8twmshiOY8",
-    authDomain: "challenge-3e4a1.firebaseapp.com",
-    projectId: "challenge-3e4a1",
-    storageBucket: "challenge-3e4a1.appspot.com",
-    messagingSenderId: "773868411413",
-    appId: "1:773868411413:web:a9d4a8a536fbce608c0c1c"
-  };
+  apiKey: "AIzaSyCsBH-lqihwtIiG4zf19K6vQ8twmshiOY8",
+  authDomain: "challenge-3e4a1.firebaseapp.com",
+  projectId: "challenge-3e4a1",
+  storageBucket: "challenge-3e4a1.appspot.com",
+  messagingSenderId: "773868411413",
+  appId: "1:773868411413:web:a9d4a8a536fbce608c0c1c",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
